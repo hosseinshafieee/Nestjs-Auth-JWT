9410953a3473f351653a3a63138799500372af75
hosseinshafieee <abedihossein700@gmail.com>
2022-02-18 14:41:55 +0330

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path/posix';
 import { AuthModule } from './auth/auth.module';
 
+
 @Module({
-  imports: [AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }), AuthModule],
 })
-export class AppModule {}
+export class AppModule { }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { AuthResolver } from './auth.resolver';
+
 
 @Module({
-  controllers: [AuthController],
-  providers: [AuthService]
+  providers: [ AuthResolver]
 })
 export class AuthModule {}
