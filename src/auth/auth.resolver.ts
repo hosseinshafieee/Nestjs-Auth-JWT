117e2fab4b73d42a5b4e6981b97e882144f3379c
hosseinshafieee <abedihossein700@gmail.com>
2022-02-19 15:58:53 +0330

    mongoose import

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service';
 import { AuthDto } from './dto/login.dto';
 import { RegisterDto } from './dto/register.dto'
 
+
 @Resolver()
 export class AuthResolver {
     constructor(private authService: AuthService) { }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Users, UsersDocument } from './Schemas/users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
+    constructor(@InjectModel(Users.name) private usersModel: Model<UsersDocument>) { }
+
     Login(email: String, password: String) {
         return email;
     }
@@ -9,4 +14,5 @@ export class AuthService {
     Register(name: String, email: String, password: String) {
         return 'test'
     }
+    
 }
