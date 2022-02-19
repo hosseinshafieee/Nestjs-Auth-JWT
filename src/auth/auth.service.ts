aa3440ff727e8d1983cb5313a5a621d6347c90ae
hosseinshafieee <abedihossein700@gmail.com>
2022-02-19 16:36:23 +0330

    insert usre mongoose

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Users, UsersDocument } from './Schemas/users.schema';
+import { Users, UsersDocument, UsersSchema } from './Schemas/users.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
-    constructor(@InjectModel(Users.name) private usersModel: Model<UsersDocument>) { }
+    constructor(@InjectModel('users') private usersModel: Model<UsersDocument>) { }
 
     Login(email: String, password: String) {
         return email;
     }
-
-    Register(name: String, email: String, password: String) {
-        return 'test'
-    }
     
+    async Register(name: String, email: String, password: String) {
+        try {
+            const user = await this.usersModel.create({ email, name, password })
+            return 'successful';
+        } catch (e) {
+            return e;
+        }
+    }
+
 }
