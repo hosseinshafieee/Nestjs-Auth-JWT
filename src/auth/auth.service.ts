45d6fedc7fbfccefd47aaa536a2f7e91a954d437
hosseinshafieee <abedihossein700@gmail.com>
2022-02-19 16:38:46 +0330

    ..

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Users, UsersDocument, UsersSchema } from './Schemas/users.schema';
+import { Users, UsersDocument } from './Schemas/users.schema';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
